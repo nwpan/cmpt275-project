77e5d34ac040577352e39efeed7f73b472f1af63
Nicholas Pan <nwpan@sfu.ca>
2012-10-01 11:44:43 -0700

    build app framework

---
+++ iRemember/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iRemember
+//
+//  Created by Nicholas Pan on 10/1/12.
+//  Copyright (c) 2012 Group 11. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iRemember/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iRemember
+//
+//  Created by Nicholas Pan on 10/1/12.
+//  Copyright (c) 2012 Group 11. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
