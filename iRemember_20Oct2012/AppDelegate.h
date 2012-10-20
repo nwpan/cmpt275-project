b426697055c39515fac7c18bcf2f98c03378f033
Nicholas <Pan>
2012-10-20 16:11:58 -0700

    misc

--- iRemember_20Oct2012/AppDelegate.h
+++ iRemember_20Oct2012/AppDelegate.h
@@ -23,4 +23,6 @@
 @property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
 @property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 
+- (void)saveContext;
+
 @end
