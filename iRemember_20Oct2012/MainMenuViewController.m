0383c34f537f1ade37b3b708953c82ec7ba04a8f
charles <sashin86@gmail.com>
2012-10-22 10:08:26 +0100

    Comment mainmenu

--- iRemember_20Oct2012/MainMenuViewController.m
+++ iRemember_20Oct2012/MainMenuViewController.m
@@ -1,10 +1,26 @@
-//
-//  MainMenuViewController.m
-//  iRemember_20Oct2012
-//
-//  Created by Steven Tjendana on 10/20/12.
-//  Copyright (c) 2012 Steven Tjendana. All rights reserved.
-//
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * MainMenuViewController.h
+ * iRemember_20Oct2012
+ *
+ * Created by Steven Tjendana on 10/20/12.
+ * Copyright (c) 2012 Double One. All rights reserved.
+ *
+ * Programmer: Steven Tjendana
+ * Team Name: Double One
+ * Project Name: iRemember
+ * Version: Version 1.0
+ *
+ * This manages main menu of our application
+ *
+ * Changes:
+ *   2012-10-20 Created
+ *   2012-10-21 Add comments
+ *
+ * Know bugs: No bugs
+ *
+ *
+ * Last revised on 2012-10-21
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 #import "MainMenuViewController.h"
 
