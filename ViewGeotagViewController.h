c284088318d0759ad182cf8d70363e78a0b685ff
Jake Nagazine <jnagazin@wmfmac05.ucslabs.sfu.ca>
2012-11-10 13:18:41 -0800

    Geotag UI

---
+++ ViewGeotagViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewGeotagViewController.h
+//  iRemember
+//
+//  Created by Jake Nagazine on 11/10/12.
+//  Copyright (c) 2012 Double One. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewGeotagViewController : UIViewController
+
+@end
